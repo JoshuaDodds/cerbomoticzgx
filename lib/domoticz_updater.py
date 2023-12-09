ae76f683a1b8e4039f68a778b8b8fad259f2316a
JoshuaDodds <jdodds@local>
2023-12-09 13:04:50 +0100

    refactor logging, build new release

--- lib/domoticz_updater.py
+++ lib/domoticz_updater.py
@@ -5,32 +5,34 @@ from lib.constants import DzEndpoints, logging, systemId0, mqtt_msg_value_conver
 
 http = urllib3.PoolManager(num_pools=10, maxsize=25)
 
+def handle_response(response, logmsg, topic):
+    code = response.status or "Unknown"
+
+    if code == 200:
+        logging.debug(f"dz_updater: {logmsg} (HTTP: {code})")
+    else:
+        logging.info(f"Timeout while attempting to update Domoticz with data from {topic}. (HTTP: {code})")
+
 
 def domoticz_update(topic, value, logmsg):
     # apply value conversions for domoticz if needed
     if mqtt_msg_value_conversion.get(get_topic_key(topic)):
         value = mqtt_msg_value_conversion.get(get_topic_key(topic))(value=value)
 
+    # It's an update from the victron integration
     if systemId0 in topic:
         try:
             _response = http.request('GET', f"{DzEndpoints['system0'][topic]}{value}")
-
-            if _response.status == 200:
-                logging.debug(f"dz_updater: {logmsg}")
-            else:
-                logging.info(f"Timeout while attempting to update Domoticz with data from {topic}.")
+            handle_response(_response, logmsg, topic)
 
         except Exception as E:
             logging.info(f"dz_updater (ERROR): {E}")
 
+    # It's an update from the Tesla integration
     else:
         try:
             _response = http.request('GET', f"{DzEndpoints['vehicle0'][topic]}{value}")
-
-            if _response.status == 200:
-                logging.debug(f"dz_updater: {logmsg}")
-            else:
-                logging.info(f"Timeout while attempting to update Domoticz with data from {topic}.")
+            handle_response(_response, logmsg, topic)
 
         except Exception as E:
             logging.info(f"dz_updater (ERROR): {E}")
