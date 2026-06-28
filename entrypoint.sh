b82a323a0fe242bdf403eb96ba565af11289f6d8
JoshuaDodds <none@none.local>
2026-06-28 08:59:10 +0200

    fix: ep script startup failure

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
-# sync any required secrets from secrets mount
-cp -a /secrets/.env* /secrets/.secrets* /secrets/* /app || true
-
-### Start the "services"
+APP_DIR="${APP_DIR:-/app}"
 
 # fork cerbomoticzGx service and restart on exit
 while true; do
-  cd /app || false && python3 main.py;
+  cd "$APP_DIR" || false && python3 main.py;
   sleep 10s;
   echo "cerbomoticzgx: service exit. restarting...";
 done &
+main_loop_pid="$!"
 
 # fork an hourly run of the tibber graphing service
 while true; do
-  cd /app || false && python3 -m lib.generate_tibber_visual | ts %Y-%m-%d" "%H:%M:%S;
+  cd "$APP_DIR" || false && python3 -m lib.generate_tibber_visual | ts %Y-%m-%d" "%H:%M:%S;
   sleep 1h;
 done &
+graph_loop_pid="$!"
+
+trap 'kill "$main_loop_pid" "$graph_loop_pid" 2>/dev/null; wait "$main_loop_pid" "$graph_loop_pid" 2>/dev/null' INT TERM EXIT
 
 # start the gitops controller
 # /app/sgc-simple-gitops-controller.sh | ts %Y-%m-%d" "%H:%M:%S
+
+wait "$main_loop_pid" "$graph_loop_pid"
